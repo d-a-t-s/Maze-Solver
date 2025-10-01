ac0a111321c07da58589a48b886099d9eb70335a
Matias Figueroa <matfigueroa2023@udec.cl>
2025-09-30 23:21:02 -0300

    se agrega la visualizacion de A*

--- src/visual_A_star.py
+++ src/visual_A_star.py
@@ -20,20 +20,20 @@ YELLOW = (255,255,0)
 FAKE = (142,112,65)
 GRAY = (128, 128, 128)
 
-cell_w = WIN_SIZE // WIDTH
-cell_h = WIN_SIZE // HEIGHT
+
+maze = Maze(21,0.05,0.05,random.randint(0,1000))
+steps = A_star.A_star(maze.pos_inicial, maze.goals, maze)
+
+cell_w = WIN_SIZE // maze.width
+cell_h = WIN_SIZE // maze.height
 cell = min(cell_w, cell_h)
-
-
-maze = maze_generator()
-steps = A_star.A_star(pos_inicial, goals, maze)
 
 # Creación de la pantalla y dibujar el laberinto
 screen = pygame.display.set_mode((WIN_SIZE, WIN_SIZE))
 pygame.display.set_caption("Maze")
 
 # Dibujar el laberinto en pantalla
-def draw_maze(maze):
+def draw_maze(maze, maze_obj):
     for row in range(len(maze)):
         for col in range(len(maze[1])):
             if maze[row][col] == -1:
@@ -42,10 +42,10 @@ def draw_maze(maze):
                 pygame.draw.rect(screen, RED, (col * cell, row * cell, cell, cell))
             elif maze[row][col] == 0:
                 pygame.draw.rect(screen, BLUE, (col * cell, row * cell, cell, cell))
-            elif (row,col) == pos_inicial:
+            elif (row,col) == maze_obj.pos_inicial:
                 pygame.draw.rect(screen, L_BLUE, (col * cell, row * cell, cell, cell))
-            elif (row,col) in goals:
-                if (row,col) == true_goal:
+            elif (row,col) in maze_obj.goals:
+                if (row,col) == maze_obj.true_goal:
                     pygame.draw.rect(screen, GREEN, (col * cell, row * cell, cell, cell))
                 else:
                     pygame.draw.rect(screen, DARK_GREEN, (col * cell, row * cell, cell, cell))
@@ -66,13 +66,13 @@ def main():
                 running = False
 
         try:
-            maze, pos = next(steps)  # obtenemos el siguiente estado
+            maze_grid, pos = next(steps)  # obtenemos el siguiente estado
         except StopIteration:
             running = False
             continue  # salimos del loop
 
         screen.fill(GRAY)
-        draw_maze(maze)
+        draw_maze(maze_grid, maze)
 
         # resaltamos la posición actual en amarillo
         pygame.draw.rect(screen, YELLOW, (pos[1]*cell, pos[0]*cell, cell, cell))
